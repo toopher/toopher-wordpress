73c772101c380e0397d5c10cbf345e24a22b9769
Drew Shafer <drew@toopher.com>
2013-06-07 15:14:30 -0500

    Store pending login info in transient instead of user options

--- lib/toopher-authenticate-login.php
+++ lib/toopher-authenticate-login.php
@@ -40,8 +40,8 @@ function toopher_finish_authenticate_login($user){
             $_POST[$toopher_key] = strip_wp_magic_quotes($_POST[$toopher_key]);
         }
 
-        $pending_session_token = get_user_meta((int)$pending_user_id, 't2s_authentication_session_token', true);
-        delete_user_meta((int)$pending_user_id, 't2s_authentication_session_token');
+        $pending_session_token = get_transient($pending_user_id . '_t2s_authentication_session_token');
+        delete_transient($pending_user_id . '_t2s_authentication_session_token');
         if(($pending_session_token === $_POST['session_token']) && ToopherWeb::validate($secret, $_POST, 100)){
             error_log('toopher signature validates');
             $authGranted = ($_POST['pending'] === 'false') && ($_POST['granted'] === 'true');
@@ -65,7 +65,7 @@ function toopher_login_pending($user){
     $baseUrl = get_site_option('toopher_api_url');
     $automatedLoginAllowed = get_site_option('toopher_allow_automated_login', true);
     $session_token = wp_generate_password(12, false);
-    update_user_meta((int)$user->ID, 't2s_authentication_session_token', $session_token);
+    set_transient($user->ID . '_t2s_authentication_session_token', $session_token, 2 * MINUTE_IN_SECONDS);
     $signed_url = ToopherWeb::auth_iframe_url($user->user_login, 'Log In', 100, $automatedLoginAllowed, $baseUrl, $key, $secret, $session_token);
 
     $toopher_finish_authenticate_parameters = array(
